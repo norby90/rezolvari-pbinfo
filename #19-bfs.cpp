07ce2426e4a784d807a20e30f3aaeb1c4cfe60ee
n0rby90 <95233772+norby90@users.noreply.github.com>
2026-07-23 15:51:54 +0300

    Add files via upload

---
+++ #19-bfs.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+using namespace std; 
+ifstream in("BFS.in");
+ofstream out("BFS.out");
+
+int main() 
+{
+    int n, m, x, i, a[105][105], viz[105], c[105], p, u, v1, v2;
+    in >> n >> m >> x;
+    for (i = 1; i <= m; i++) 
+    {
+        in >> v1 >> v2;
+        a[v1][v2] = a[v2][v1] = 1;
+    } 
+    p = 1; u = 1; c[p] = x; viz[x] = 1;
+    while (p <= u) 
+    {
+        for (i = 1; i <= n; i++)
+            if (a[c[p]][i] == 1 && viz[i] == 0) 
+            {
+		        u++; c[u] = i; viz[i] = 1;
+	        } 
+        p++;
+    } 
+    for (i = 1; i <= u; i++) 
+    {
+        out << c[i] << " ";
+    }
+
+    return 0;
+}
